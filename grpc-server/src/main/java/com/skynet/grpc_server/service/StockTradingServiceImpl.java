421c5f411e7dfcdf3c377c05b25589c8e150f9e0
mymborrini <mattia.borrini@adesso.ch>
2025-07-28 10:40:40 +0200

    feat(Grpc Sever Streaming): Grpc Server
    Implements: the grpc server using spring boot with stream server

--- grpc-server/src/main/java/com/skynet/grpc_server/service/StockTradingServiceImpl.java
+++ grpc-server/src/main/java/com/skynet/grpc_server/service/StockTradingServiceImpl.java
@@ -6,26 +6,31 @@ import com.skynet.grpc_server.StockResponseOrBuilder;
 import com.skynet.grpc_server.StockTradingServiceGrpc;
 import com.skynet.grpc_server.model.Stock;
 import io.grpc.stub.StreamObserver;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.grpc.server.service.GrpcService;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @GrpcService
+@Slf4j
 public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradingServiceImplBase {
 
 
-  Map<String, Stock> stocks = new HashMap<>();
+  Map<String, Stock> stocks = Map.of(
+          "GOOGLE", new Stock().setPrice(24.1).setTimeStamp("12345635"),
+          "APPLE", new Stock().setPrice(33.3).setTimeStamp("987654332"),
+          "TESLA", new Stock().setPrice(15.6).setTimeStamp("123987045")
+  );
 
   // The method returns void. So we need to work with StreamObserver. If you look inside StreamObserver interface
   // it works more or less like a node callback class. It has onNext, onError, onComplete method
   @Override
   public void getStockPrice(StockRequest request, StreamObserver<StockResponse> responseObserver) {
-
-    // Initialize Stocks
-    stocks.put("GOOGLE", new Stock().setPrice(24.1).setTimeStamp("12345635"));
-    stocks.put("APPLE", new Stock().setPrice(33.3).setTimeStamp("987654332"));
-    stocks.put("TESLA", new Stock().setPrice(15.6).setTimeStamp("123987045"));
 
     // In the request you find stockSymbol -> get if from the map response -> return
 
@@ -36,6 +41,30 @@ public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradin
 
     responseObserver.onNext(stockResponse);
     responseObserver.onCompleted();
+
+  }
+
+  @Override
+  public void subscribeStockPrice(StockRequest request, StreamObserver<StockResponse> responseObserver) {
+    String stockSymbol = request.getStockSymbol();
+
+    try {
+      for (int i = 0; i < 10; i++) {
+        StockResponse stockResponse = StockResponse.newBuilder()
+                .setStockSymbol(stockSymbol)
+                .setPrice(new Random().nextDouble(200))
+                .setTimestamp(Instant.now().toString())
+                .build();
+        responseObserver.onNext(stockResponse);
+
+        // This is used to give the effect of a server response
+        TimeUnit.SECONDS.sleep(1);
+      }
+    } catch (InterruptedException e) {
+      log.error(e.getMessage());
+      responseObserver.onError(e);
+    }
+
 
   }
 }
